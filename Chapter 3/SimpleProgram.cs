9e213cbb0d53fbe09e3136d683e3cb4df9fbba94
yanyang <yanyang@DESKTOP-PDQ6L3F>
2023-12-07 17:19:59 +0800

    3-1

---
+++ Chapter 3/SimpleProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple    //命名空间
+{
+    class SimpleProgram   //声明一个类
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi theree!");
+        }
+    }
+}
