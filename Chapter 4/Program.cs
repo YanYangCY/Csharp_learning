5f65bd42935a4cff47ed488266823c058f3a5251
yanyang <yanyang@DESKTOP-PDQ6L3F>
2023-12-27 13:18:37 +0800

    命名规则及第四章的初步学习

--- Chapter 4/Program.cs
+++ Chapter 4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Chapter4
+{
+    /// <summary>
+    /// 1.与C和C++不同，C#中的数值类型不具有布尔意义
+    /// 2.预定义类型(16)：简单类型（数值、非数值）、object、string、dynamic
+    /// 3.自定义类型(6)：类（class）、结构（struct）、数组（array）、枚举（enum）、委托（delegate）、接口（interface）
+    /// </summary>
+    class chapter4
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
