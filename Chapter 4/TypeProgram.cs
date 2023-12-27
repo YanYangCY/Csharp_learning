a6d834d216d5bab7b9af296659e0d4742052dc3a
yanyang <yanyang@DESKTOP-PDQ6L3F>
2023-12-27 14:32:31 +0800

    更改第四章项目名称

--- Chapter 4/TypeProgram.cs
+++ Chapter 4/TypeProgram.cs
@@ -7,6 +7,8 @@ namespace Chapter4
     /// 1.与C和C++不同，C#中的数值类型不具有布尔意义
     /// 2.预定义类型(16)：简单类型（数值、非数值）、object、string、dynamic
     /// 3.自定义类型(6)：类（class）、结构（struct）、数组（array）、枚举（enum）、委托（delegate）、接口（interface）
+    /// 4.预定义只需要实例化、自定义的需要声明和实例化
+    /// 5.※4-8中的内存存放位置还有疑问，后面再看
     /// </summary>
     class chapter4
     {
@@ -14,5 +16,13 @@ namespace Chapter4
         {
 
         }
+    }
+    /// <summary>
+    /// 通过静态类和静态成员实现全局变量的效果
+    /// 尽可能将数据封装到类的实例中，通过对象的方式进行操作和访问
+    /// </summary>
+    public static class GlobalVariables
+    {
+        public static int GlobalVariable = 10;
     }
 }
