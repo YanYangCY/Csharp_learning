58d0a9e32a702f39308c665459fb838635cb7f67
yanyang <yanyang@DESKTOP-PDQ6L3F>
2024-01-07 11:02:58 +0800

    第五章：类的字段和方法

---
+++ Chapter5/ClassConcept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassConcept
+{
+    /// <summary>
+    /// 字段是隶属于类的变量（可读写）
+    /// C#中没有默认返回类型，所有方法必须包含返回类型或void
+    /// </summary>
+    class ClasConcept
+    {
+        static void Main()
+        {
+            Console.WriteLine("chapter 5");
+        }
+    }
+}
