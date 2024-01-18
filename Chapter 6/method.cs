5bbc87a133923dcdc42b67cdd25383cb1f775fd6
yanyang <yanyang@DESKTOP-PDQ6L3F>
2024-01-18 09:12:44 +0800

    第六章开始

---
+++ Chapter 6/method.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Method
+{
+    /// <summary>
+    /// 方法头：访问修饰符、静态修饰符、返回类型、方法名、
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
