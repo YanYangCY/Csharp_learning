e288679c09199ec4bdd379c89ebae915b0d1fad8
yanyang <yanyang@DESKTOP-PDQ6L3F>
2024-01-19 15:22:45 +0800

    第六章 方法体中的应用

--- Chapter 6/method.cs
+++ Chapter 6/method.cs
@@ -7,17 +7,35 @@ namespace Method
     /// 方法体：局部变量、控制流结构(条件、循环、跳转)、方法调用、内嵌的块、其他方法(局部函数)
     /// var关键字只能用于局部变量，不能用于字段(类、结构体的变量)；只能在变量声明中包含初始化时使用
     /// const关键字用于声明常量类型
+    /// 在void声明的方法中，可以使用return;来退出方法
     /// </summary>
-    class Program
+    class MethodTest
     {
+        public void TestReturn()
+        {
+            int x = 1;
+            int y = 2;
+            if (x == 1) { Console.WriteLine("X: {0}", x); return; } //return语句后面的语句不会执行
+            if (y == 2) { Console.WriteLine("Y: {0}", y); }
+        }
+
         static void Main(string[] args)
         {
+            var newMetod = new MethodTest();    //实例化对象，调用对象的方法
+
             int a = 1;
             a = (int)(double)a; //类型转换，必须先转换为double，再转换为int
             var b = (double)a; //自动推断类型，编译器会根据变量的赋值语句推断变量的类型
+            Console.WriteLine("******************************");
             Console.WriteLine("{0}",a.GetType());
             Console.WriteLine("{0}", b.GetType());
+            Console.WriteLine("******************************");
+
+            newMetod.TestReturn();  //调用对象的方法
+            Console.WriteLine("******************************");
+
 
         }
+        
     }
 }
