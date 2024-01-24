17f22e269b7dc80388dfa439b7edb2f95af7d4dc
yanyang <yanyang@DESKTOP-PDQ6L3F>
2024-01-24 17:29:35 +0800

    修饰符ref、out、params学习

--- Chapter 6/method.cs
+++ Chapter 6/method.cs
@@ -9,7 +9,8 @@ namespace Method
     /// var关键字只能用于局部变量，不能用于字段(类、结构体的变量)；只能在变量声明中包含初始化时使用
     /// const关键字用于声明常量类型
     /// 在void声明的方法中，可以使用return;来退出方法
-    /// 使用ref关键字传递方法参数，方法接收到的都是直接内存引用；方法的声明和调用上都需要使用ref
+    /// 使用ref修饰符传递方法参数，方法接收到的都是直接内存引用；方法的声明和调用上都需要使用ref
+    /// 使用out修饰符，在方法内部给输出参数赋值之后才能读取它
     /// </summary>
     class MethodTest
     {
@@ -29,6 +30,16 @@ namespace Method
             Console.WriteLine($"f1.Val:{f1.Val}, f2:{f2}");
         }
 
+        public void ListInts(params int[] inVals)
+        {
+            if((inVals != null) && (inVals.Length != 0))
+                for (int i = 0; i < inVals.Length; i++)
+                {
+                    inVals[i] = inVals[i] * 10;
+                    Console.WriteLine($"{inVals[i]}");
+                }
+        }
+
         static void Main(string[] args)
         {
             var newMetod = new MethodTest();    //实例化对象，调用对象的方法
@@ -46,9 +57,20 @@ namespace Method
 
             newMetod.MyMethod(ref newMetod, ref a); //调用对象的方法，使用ref关键字，将方法的参数传递为引用类型
             Console.WriteLine($"newMetod.Val:{newMetod.Val}, a:{a}");
-        
+            Console.WriteLine("******************************");
 
-
+            //将值类型的数据作为数组参数，值被复制，实参在方法内部不受影响
+            int first = 5, second = 6, third = 7;
+            var listMethod = new MethodTest();
+            listMethod.ListInts(first, second, third);
+            Console.WriteLine($"{first},{second},{third}");
+            //将数组作为实参，编译器会使用数组，而不是重新创建
+            int[] myArr = new int[] {5, 6, 7};
+            var listMethod2 = new MethodTest();
+            listMethod2.ListInts(myArr);
+            foreach (int x in myArr)
+                Console.WriteLine($"{ x }");
+            Console.WriteLine("******************************");
         }
         
     }
---
+++ YY/Qualifier-修饰符.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YY
+{
+    /// <summary>
+    /// 修饰符大全
+    /// (* 声明调用时都需要使用)
+    /// 1. ref*         用于传递参数时指定按引用传递
+    /// 2. out*         用于方法参数传递时表明该参数是一个输出参数
+    /// 3. params       用于方法的参数列表中定义可变数量的参数
+    /// </summary>
+    internal class Qualifier
+    {
+
+    }
+}
