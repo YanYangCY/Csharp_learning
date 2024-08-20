e71c1e86e259cf7c9cf8954d99472974d7b0cb99
阳 严 <2582960451@qq.com>
2024-08-20 17:30:27 +0800

    第十章语句-while循环

--- Chapter 10/Statement.cs
+++ Chapter 10/Statement.cs
@@ -1,10 +1,39 @@
 ﻿namespace Chapter_10
 {
+    /// <summary>
+    /// 10.1 什么是语句
+    ///     1.声明语句：声明类型或变量
+    ///     2.嵌入语句：执行动作或管理控制流
+    ///     3.标签语句：控制跳转
+    /// 10.2 表达式语句
+    /// 10.3 控制流语句
+    ///     1.条件执行依据一个条件执行或跳过一个代码片段。条件执行语句如下：
+    ///         if
+    ///         if...else
+    ///         switch
+    ///     2.循环语句重复执行一个代码片段。循环语句如下：
+    ///         while
+    ///         do
+    ///         for
+    ///         foreach
+    ///     3.跳转语句把控制流从一个代码片段改变到另一个代码片段中的指定语句。跳转语句如下：
+    ///         break
+    ///         continue
+    ///         return
+    ///         goto
+    ///         throw
+    /// 10.4 if语句
+    ///     if(TestExpr) Statement  //TestExpr必须计算成bool型值；如果TestExpr求值为true，执行Statement；如果求值为false，则跳过Statement
+    /// 10.5 if...else语句
+    ///     if(TestExpr) Statement1 else Statement2  //TestExpr必须计算成bool型值；如果TestExpr求值为true，执行Statement1；如果求值为false，则执行Statement2
+    /// 10.6 while循环
+    ///     while(TestExpr) Statement   //如果TestExpr求值为true，执行Statement；如果求值为false，则执行while循环结束之后的语句
+    /// </summary>
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
