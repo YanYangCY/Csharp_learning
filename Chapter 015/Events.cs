da6daa26e597988515f5331aba49fdfd76264a1b
阳 严 <2582960451@qq.com>
2024-09-24 17:26:05 +0800

    第15章-事件开始

---
+++ Chapter 015/Events.cs
@@ -0,0 +1,14 @@
+﻿namespace Chapter_015
+{
+    /// <summary>
+    /// 15.1 发布者和订阅者
+    ///     
+    /// </summary>
+    internal class Events
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
