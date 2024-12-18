67358d7ae7903416a338ab947eb09f960ce83001
阳 严 <2582960451@qq.com>
2024-12-18 17:28:10 +0800

    迭代器的使用

--- Chapter 019/Traversal.cs
+++ Chapter 019/Traversal.cs
@@ -23,12 +23,19 @@ namespace Chapter_019   // 枚举器和迭代器
     ///         如果新的位置有效，返回ture；无效返回false
     ///         枚举器的原始位置在序列中的第一项之前，因此MoveNext必须在第一次使用Current之前调用
     ///     3.Reset是把位置重置为原始状态的方法
-    /// 19.3        
+    /// 19.3 IEnumerable接口
+    ///     可枚举类是指实现了IEnumerable接口的类。IEnumerable接口只有一个成员---GetEnumerator方法，它返回对象的枚举器。
+    /// 19.4 泛型枚举接口        
+    ///     目前描述的接口都是非泛型，大多数情况下会使用泛型版本IEnumberable<T>和IEnumerator<T>。
+    /// 19.5 迭代器   
+    ///     迭代器(iterator)提供了更简单的创建枚举器和可枚举类型的方式(实际上迭代器创建了它们)
+    ///     19.5.1 迭代器块
+    ///         迭代器块是有一个或多个yield语句的代码块。迭代器块：方法主体、访问器主体、运算符主体
+    ///         迭代器块有两个特殊语句：
+    ///             yield return 语句指定了序列中要返回的下一项；
+    ///             yield break 语句指定在序列中没有其他项。
+    ///     19.5.2 使用迭代器来创建枚举器
     ///         
-    /// 
-    /// 
-    /// 
-    /// 
     /// 
     /// 
     /// 
@@ -52,7 +59,84 @@ namespace Chapter_019   // 枚举器和迭代器
                 Console.WriteLine($"Item Value: {item}");
             }
             Console.WriteLine("####################################");
-
+            Console.WriteLine("######可枚举类的示例#################");
+            Specturm specturm = new Specturm();
+            foreach (string color in specturm)
+                Console.WriteLine(color);
+            Console.WriteLine("####################################");
+            Console.WriteLine("######使用迭代器来创建枚举器##########");
+            IteratorCreateEnumerator iterator = new IteratorCreateEnumerator();
+            foreach ( string data in iterator)
+                Console.WriteLine(data);
+            Console.WriteLine("####################################");
         }
     }
+    #region 可枚举类的示例---使用IEnumerable和IEnumerator
+    /// <summary>
+    /// 定义一个ColorEnumerator类，它实现了IEnumerator接口，用于枚举颜色字符串数组
+    /// </summary>
+    class ColorEnumerator : IEnumerator
+    {
+        string[] colors;    // 私有字段，用于存储颜色字符串数组
+        int position = -1;  // 私有字段，表示当前枚举器的位置，-1表示枚举器位于初始位置（即，尚未开始枚举）
+        public ColorEnumerator(string[] theColors)  // 构造函数,接收一个颜色字符串数组作为参数，并将其赋值给colors字段
+        {
+            colors = new string[theColors.Length];
+            for (int i = 0; i < theColors.Length; i++)
+                colors[i] = theColors[i];
+        }
+        public object Current   // 实现Current,返回当前枚举的元素
+        {
+            get
+            {
+                if (position == -1) // 如果枚举器尚未开始枚举，则抛出InvalidOperationException异常
+                    throw new InvalidOperationException();  
+                if(position >= colors.Length)   // 如果枚举器已超出数组末尾，则也抛出InvalidOperationException异常
+                    throw new InvalidOperationException();
+                return colors[position];     // 返回当前位置的颜色字符串
+            }
+        }
+        public bool MoveNext()  // 实现MoveNext，将枚举器推进到下一个元素
+        {
+            if (position < colors.Length - 1)   // 如果当前位置不是数组的最后一个元素，则将位置加1并返回true
+            {
+                position++;
+                return true;
+            }
+            else     // 如果当前位置是数组的最后一个元素或已超出末尾，则返回false
+                return false;
+        }
+        public void Reset() // 实现Reset，将枚举器重置到其初始位置
+        {
+            position = -1;  // 将位置重置为-1，表示枚举器尚未开始枚举
+        }
+    }
+    class Specturm : IEnumerable    // 实现IEnumerable接口
+    {
+        string[] colors = { "Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet" };  // 私有字段，存储颜色字符串数组
+        public IEnumerator GetEnumerator()  // 实现IEnumerable接口的GetEnumerator方法，返回一个用于枚举Specturm中颜色的枚举器
+        {
+            // 创建一个ColorEnumerator实例，并传入colors数组作为参数
+            // 然后返回这个枚举器实例，以便外部代码可以使用它来枚举颜色
+            return new ColorEnumerator(colors);
+        }
+    }
+    #endregion
+
+    #region 使用迭代器来创建枚举器
+    class IteratorCreateEnumerator
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            return BlackAndWhite(); // 返回枚举器
+        }
+        public IEnumerator<string> BlackAndWhite()  // 迭代器
+        {
+            yield return "Black";
+            yield return "Gray";
+            yield return "White";
+        }
+    }
+    #endregion
+
 }
